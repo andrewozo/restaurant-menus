d12f493247c4c1ba5b25ba38d71d13d44dc1a689
Andrew <andrewozo123@gmail.com>
2023-11-15 13:50:05 -0500

    finished extension

--- index.test.js
+++ index.test.js
@@ -1,43 +1,83 @@
-const {sequelize} = require('./db')
-const {Restaurant, Menu} = require('./models/index')
-const {
-    seedRestaurant,
-    seedMenu,
-  } = require('./seedData');
+const { sequelize } = require("./db");
+const { Restaurant, Menu } = require("./models/index");
+const { seedRestaurant, seedMenu } = require("./seedData");
 
-describe('Restaurant and Menu Models', () => {
-    /**
-     * Runs the code prior to all tests
-     */
-    beforeAll(async () => {
-        // the 'sync' method will create tables based on the model class
-        // by setting 'force:true' the tables are recreated each time the 
-        // test suite is run
-        await sequelize.sync({ force: true });
+describe("Restaurant and Menu Models", () => {
+  /**
+   * Runs the code prior to all tests
+   */
+  beforeAll(async () => {
+    // the 'sync' method will create tables based on the model class
+    // by setting 'force:true' the tables are recreated each time the
+    // test suite is run
+    // await Restaurant.bulkCreate(seedRestaurant);
+    // await Menu.bulkCreate(seedMenu);
+    await sequelize.sync({ force: true });
+  });
+
+  test("can create a Restaurant", async () => {
+    // TODO - write test
+    const restaurant = await Restaurant.create({
+      name: "Drews Kitchen",
+      location: "queens,ny",
+      cuisine: "new york",
     });
 
-    test('can create a Restaurant', async () => {
-        // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
+    expect(restaurant).toEqual(
+      expect.objectContaining({
+        name: "Drews Kitchen",
+        location: "queens,ny",
+        cuisine: "new york",
+      })
+    );
+  });
+
+  test("can create a Menu", async () => {
+    // TODO - write test
+    const menu = await Menu.create({
+      title: "Drews Menu",
     });
 
-    test('can create a Menu', async () => {
-        // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
-    });
+    expect(menu).toEqual(
+      expect.objectContaining({
+        title: "Drews Menu",
+      })
+    );
+  });
 
-    test('can find Restaurants', async () => {
-        // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
-    });
+  test("can find Restaurants", async () => {
+    // TODO - write test
+    await Restaurant.bulkCreate(seedRestaurant);
+    const restaurant = await Restaurant.findByPk(1);
 
-    test('can find Menus', async () => {
-        // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
-    });
+    console.log("restaurant: ", restaurant);
+    expect(restaurant).toEqual(
+      expect.objectContaining({
+        name: "Drews Kitchen",
+        location: "queens,ny",
+        cuisine: "new york",
+      })
+    );
+  });
 
-    test('can delete Restaurants', async () => {
-        // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
-    });
-})
+  test("can find Menus", async () => {
+    // TODO - write test
+    await Menu.bulkCreate(seedMenu);
+    const menu = await Menu.findByPk(1);
+    expect(menu).toEqual(
+      expect.objectContaining({
+        title: "Drews Menu",
+      })
+    );
+  });
+
+  test("can delete Restaurants", async () => {
+    // TODO - write test
+    await Restaurant.bulkCreate(seedRestaurant);
+    const restaurant = await Restaurant.findByPk(2);
+    await restaurant.destroy();
+    const deletedRestaurant = await Restaurant.findByPk(2);
+
+    expect(deletedRestaurant).toEqual(null);
+  });
+});
--- models/Restaurant.js
+++ models/Restaurant.js
@@ -1,6 +1,12 @@
-const {sequelize} = require('../db');
-const { Sequelize } = require('sequelize');
+const { sequelize } = require("../db");
+const { Sequelize, DataTypes } = require("sequelize");
 
 // TODO - create a Restaurant model
 
-module.exports = {Restaurant};
+const Restaurant = sequelize.define("Restaurant", {
+  name: DataTypes.STRING,
+  location: DataTypes.STRING,
+  cuisine: DataTypes.STRING,
+});
+
+module.exports = { Restaurant };
